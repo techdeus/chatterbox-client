39f6f8535a53c18d003e74294c9b78dfcf4f7983
techdeus <sullivan.marlon@gmail.com>
2018-09-28 18:28:43 -0700

    Add underscore escape function call on username and text

--- client/scripts/messageView.js
+++ client/scripts/messageView.js
@@ -1,12 +1,19 @@
 var MessageView = {
-
+  
   render: _.template(`
       
       <div class="chat"> 
-        <div class="username"><%= username %>:</div> 
-        <div class="message"><%= message %></div>
+        <div class="username"><%= _.escape(username) %>:</div> 
+        <div class="message"><%= _.escape(text) %></div>
       </div>
     
-    `)
+    `, settings
+  )
 
 };
+
+var settings =  {
+  evaluate: /<%([\s\S]+?)%>/g,
+  interpolate: /<%=([\s\S]+?)%>/g,
+  escape: /<%-([\s\S]+?)%>/g
+}
