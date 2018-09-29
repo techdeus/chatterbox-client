c9ebd314de16a45d7fa373f24bc05cecab3f8fb2
techdeus <sullivan.marlon@gmail.com>
2018-09-28 18:28:04 -0700

    Add filter for messages without username and text

--- client/scripts/messagesView.js
+++ client/scripts/messagesView.js
@@ -6,13 +6,18 @@ var MessagesView = {
     // get current messages
     // call readAll to get messages
     Parse.readAll(function(messagesJSON){
-      console.log("================");
-      console.log(messagesJSON);
-      console.log("================");
+      //_.filter messages with full properties
+    
+      var filtJSONResults = _.filter(messagesJSON.results, function(messageObject) {
+        return messageObject.username && messageObject.text;
+      });
+      console.log(filtJSONResults);  
       //For each message object, 
-      _.each(messagesJSON.results, function(messageObject, i){
+      _.each(filtJSONResults, function(messageObject){
         //call render template function to create view
         // debugger;
+    
+        
         let mView = MessageView.render(messageObject);
         // append them to the chats DIV
         MessagesView.$chats.append(mView);
