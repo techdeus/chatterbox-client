f89b5b444a72f423ca5f5f6509deee0f0a837e8e
techdeus <sullivan.marlon@gmail.com>
2018-09-28 17:08:47 -0700

    Outline the handleSubmit function

--- client/scripts/formView.js
+++ client/scripts/formView.js
@@ -9,7 +9,15 @@ var FormView = {
   handleSubmit: function(event) {
     // Stop the browser from submitting the form
     event.preventDefault();
-    
+    // get the text from userinput
+    // escape characters to prevent XSS
+    // AJAX call from Parse.create, to create message
+      // check if message was a success
+        // if success
+          // render messages view
+        // if not
+          // console.log the error
+
     console.log('click!');
   },
 
