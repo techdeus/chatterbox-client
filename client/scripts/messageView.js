41a050a301d80ac9f572ada2fb008c8ee73a8605
techdeus <sullivan.marlon@gmail.com>
2018-09-28 17:06:31 -0700

    Finish render template

--- client/scripts/messageView.js
+++ client/scripts/messageView.js
@@ -1,12 +1,12 @@
 var MessageView = {
 
   render: _.template(`
-      <!--
-      <div class="chat">
-        <div class="username"></div>
-        <div></div>
+      
+      <div class="chat"> 
+        <div class="username"><%= username %>:</div> 
+        <div class="message"><%= message %></div>
       </div>
-      -->
+    
     `)
 
 };
